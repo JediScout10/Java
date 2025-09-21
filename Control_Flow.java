04b96e55ba02a990a8deede9c89bb0b6b7d8b712
JediScout10 <jedi.scout10@gmail.com>
2025-09-22 01:30:48 +0530

    new file

---
+++ Control_Flow.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Control_Flow {
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        
+        System.out.print("Enter your name: ");
+        String name = sc.next(); // single word
+        // String name = sc.nextLine(); // full line
+        
+        System.out.println("Hello " + name + ", your age is " + age);
+        
+        sc.close();
+ }    
+}
