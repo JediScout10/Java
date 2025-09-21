0a24e9f5f5f3c7f6f107715c195d79e26c8e3950
JediScout10 <jedi.scout10@gmail.com>
2025-09-22 01:39:30 +0530

    new file

--- Control_Flow.java
+++ Control_Flow.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
+
 public class Control_Flow {
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Enter your age: ");
         int age = sc.nextInt();
-        
+
         System.out.print("Enter your name: ");
         String name = sc.next(); // single word
         // String name = sc.nextLine(); // full line
-        
+
         System.out.println("Hello " + name + ", your age is " + age);
-        
+
         sc.close();
- }    
+        int a = age;
+
+        if (a >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+
+    }
 }
