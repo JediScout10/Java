a09535f158e07b439f51048b540f065eb5296b96
JediScout10 <jedi.scout10@gmail.com>
2025-09-25 05:13:31 +0000

    3rd file

---
+++ Oops.java
@@ -0,0 +1,18 @@
+// class Student{
+//         int age;
+//             String name;
+//                 int marks;
+//                     void display(){
+//                             System.out.print("name :"+name+" age: "+age+" marks: "+marks);
+//                                 }
+
+//                                 }
+//                                     public class Oops {
+//                                     public static void main(String[] args){
+//                                     Student s1= new Student();
+//                                     s1.age=20;
+//                                     s1.name="Rohit";
+//                                     s1.marks=36;
+//                                     s1.display();
+//                                     }}
+
